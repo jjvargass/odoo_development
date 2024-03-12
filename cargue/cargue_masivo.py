73a14fb8fc64646e93ebdc68a987a0abb4016dc6
jjvargass <jotavargas89@gmail.com>
2024-03-12 18:54:49 -0500

    cargue para crm

--- cargue/cargue_masivo.py
+++ cargue/cargue_masivo.py
@@ -18,6 +18,7 @@ def main():
     parser.add_argument('-n', '--db_name',     dest='db_name',     help='Odoo database name', required=True)
     parser.add_argument('-u', '--db_user',     dest='db_user',     help='Odoo database user', required=True)
     parser.add_argument('-p', '--db_password', dest='db_password', help='Odoo database password', required=True)
+    parser.add_argument('-w', '--file_way',    dest='file_way',    help='way o path of file csv', required=True)
     parser.add_argument('-j', '--host_odoo',   dest='host_odoo',   help='Odoo server host', default='http://localhost')
     parser.add_argument('-k', '--port_odoo',   dest='port_odoo',   help='Odoo server port', default='8069')
 
@@ -27,12 +28,29 @@ def main():
     #_logger.info(odoo_client.common.version())
 
     utilidad = OdooTools(odoo_client, _logger)
-    utilidad.get_user("portal",None)
+    
+    # usuarios
+    _logger.info("Usuario========")
+    usuario = utilidad.get_user("portal",None)
+    _logger.info(usuario.partnet_id)
 
+    # contacto
+    _logger.info("Contacto========")
+    contacto = utilidad.create_find_res_partner(
+        False,
+        'MINTIC - Ministerio TIC',
+        # 'Edificio Murillo Toro Cra. 8a entre calles 12A y 12B Bogotá',
+        # '+57 601 344 34 60',
+        # '',
+        # 'minticresponde@mintic.gov.co',
+        # 'https://www.mintic.gov.co/',
+    )
+    _logger.info(contacto)
 
 def get_conection(options):
     server = options.host_odoo + ':' + options.port_odoo
     return erppeek.Client(server, options.db_name, options.db_user, options.db_password)
+
 
 
 if __name__ == '__main__':
--- cargue/odoo_tools.py
+++ cargue/odoo_tools.py
@@ -12,3 +12,36 @@ class OdooTools():
         elif login:
             user = self.odoo_client.model('res.users').get([('login','=',login.strip())])
         return user
+
+    # contacto
+    def create_find_res_partner(self, is_company=False, name='', street='', phone='', mobile='', email='', website=''):
+        res_partner = self.odoo_client.model('res.partner').get([
+            ('name','=',name.strip()),
+            ('is_company','=', is_company)
+        ])
+        # pais
+        colombia = self.odoo_client.model('res.country').get([
+            ('name','=','Colombia'.strip()),
+            ('code','=', 'CO')
+        ])
+        # Ciudad
+        bogota = self.odoo_client.model('res.country.state').get([
+            ('country_id','=', colombia.id),
+            ('code','=', 'DC')
+        ])
+        self._logger.info(colombia)
+        if not res_partner:
+            self._logger.info('No Existe Contacto: ' + name + '- Se Procede a Crear')
+            res_partner = self.odoo_client.model('res.partner').create({
+                'is_company': is_company,
+                'name': name.strip(),
+                'street': street.strip(),
+                'phone': phone.strip(),
+                'mobile': mobile.strip(),
+                'email': email.strip(),
+                'website': website.strip(),
+                'country_id': colombia.id,
+                'state_id': bogota.id,
+            })
+        return res_partner
+        
