635ee294736516399ddeb876330475e8509d7441
jjvargass <jotavargas89@gmail.com>
2024-03-14 18:18:54 -0500

    cargue con lectura de csv

--- cargue/cargue_masivo.py
+++ cargue/cargue_masivo.py
@@ -6,6 +6,7 @@ import argparse
 import erppeek
 
 from cargue_tools import CargueTools
+from crm.cargue_crm_agata import CargueCrmAgata
 
 logging.basicConfig(level=logging.DEBUG)
 _logger = logging.getLogger('cargue')
@@ -20,65 +21,68 @@ def main():
     parser.add_argument('-n', '--db_name',     dest='db_name',     help='Odoo database name', required=True)
     parser.add_argument('-u', '--db_user',     dest='db_user',     help='Odoo database user', required=True)
     parser.add_argument('-p', '--db_password', dest='db_password', help='Odoo database password', required=True)
-    parser.add_argument('-w', '--file_way',    dest='file_way',    help='way o path of file csv', required=True)
+    parser.add_argument('-f', '--file_name',    dest='file_name',    help='name of file csv for upload', required=True)
     parser.add_argument('-j', '--host_odoo',   dest='host_odoo',   help='Odoo server host', default='http://localhost')
     parser.add_argument('-k', '--port_odoo',   dest='port_odoo',   help='Odoo server port', default='8069')
 
     options = parser.parse_args()
 
     odoo_client = get_conection(options)
-    #_logger.info(odoo_client.common.version())
 
-    utilidad = CargueTools(odoo_client, _logger)
-    
-    # usuarios
-    _logger.info("Usuario========")
-    usuario = utilidad.get_user("portal",None)
-    _logger.info(usuario.partnet_id)
+    cargue_crm_agata = CargueCrmAgata(odoo_client, _logger, options)
+    cargue_crm_agata.open_file_crm_agata()
 
-    # contacto
-    _logger.info("Contacto========")
-    padre_contacto = utilidad.create_find_res_partner(
-        True,
-        None,
-        'MINTIC - Ministerio TIC',
-        'Edificio Murillo Toro Cra. 8a entre calles 12A y 12B Bogotá',
-        '+57 601 344 34 60',
-        '',
-        'minticresponde@mintic.gov.co',
-        'https://www.mintic.gov.co/',
-        '',
-    )
 
-    contacto = utilidad.create_find_res_partner(
-        False,
-        padre_contacto.id,
-        'Lina Zuluaga',
-        'Edificio Murillo Piso 3',
-        '',
-        '3102422608',
-        'minticresponde@mintic.gov.co',
-        'https://www.mintic.gov.co/',
-        'Asesora de IA',
-    )
-    _logger.info(contacto)
+    # utilidad = CargueTools(odoo_client, _logger)
 
-    _logger.info("Oportunidad========")
-    oportunidad = utilidad.create_oportunidad(
-        'Redes Sociales JOTAAAAAAAA',
-        832837311,
-        20.0,
-        contacto.id,
-        'jose@xx.com',
-        '3',
-        'Perdido',
-        'Producto',
-        'Redes Sociales',
-        'Wise CX',
-        'Nación',
-        'No',
-    )
-    _logger.info(oportunidad)
+    # # usuarios
+    # _logger.info("Usuario========")
+    # usuario = utilidad.get_user("portal",None)
+    # _logger.info(usuario.partnet_id)
+
+    # # contacto
+    # _logger.info("Contacto========")
+    # padre_contacto = utilidad.create_find_res_partner(
+    #     True,
+    #     None,
+    #     'MINTIC - Ministerio TIC',
+    #     'Edificio Murillo Toro Cra. 8a entre calles 12A y 12B Bogotá',
+    #     '+57 601 344 34 60',
+    #     '',
+    #     'minticresponde@mintic.gov.co',
+    #     'https://www.mintic.gov.co/',
+    #     '',
+    # )
+
+    # contacto = utilidad.create_find_res_partner(
+    #     False,
+    #     padre_contacto.id,
+    #     'Lina Zuluaga',
+    #     'Edificio Murillo Piso 3',
+    #     '',
+    #     '3102422608',
+    #     'minticresponde@mintic.gov.co',
+    #     'https://www.mintic.gov.co/',
+    #     'Asesora de IA',
+    # )
+    # _logger.info(contacto)
+
+    # _logger.info("Oportunidad========")
+    # oportunidad = utilidad.create_oportunidad(
+    #     'Redes Sociales JOTAAAAAAAA',
+    #     832837311,
+    #     20.0,
+    #     contacto.id,
+    #     'jose@xx.com',
+    #     '3',
+    #     'Perdido',
+    #     'Producto',
+    #     'Redes Sociales',
+    #     'Wise CX',
+    #     'Nación',
+    #     'No',
+    # )
+    # _logger.info(oportunidad)
 
 if __name__ == '__main__':
     main()
--- cargue/crm/cargue_crm_agata.py
+++ cargue/crm/cargue_crm_agata.py
@@ -2,6 +2,8 @@
 #coding: utf-8
 
 import csv
+import os
+from cargue_tools import CargueTools
 
 
 class CargueCrmAgata():
@@ -10,5 +12,40 @@ class CargueCrmAgata():
         self._logger = _logger
         self.options = options
 
-    def open_crm_agata(self):
-        
+    def open_file_crm_agata(self):
+        # Ruta Actual
+        separador = os.path.sep  # obtiene segun el sistema operativo
+        dir_actual = os.path.dirname(os.path.abspath(__file__))
+
+        cargue_tools = CargueTools(self.odoo_client, self._logger)
+
+        with open(dir_actual + '/' + self.options.file_name) as csvfile:
+            reader = csv.DictReader(csvfile)
+            rowcount  = 0
+            for row in reader:
+                rowcount+= 1
+                self._logger.info('Line CSV:' + str(rowcount))
+                # crear cliente Compañia
+                cliente_compañia = cargue_tools.create_find_res_partner(
+                    True,
+                    None,
+                    row['cliente_compania_nombre'],
+                    row['cliente_compania_direccion'],
+                    row['cliente_compania_telefono'],
+                    row['cliente_compania_cel'],
+                    row['cliente_compania_email'],
+                    row['cliente_compania_web'],
+                    '',
+                )
+                if row['cliente_individuo_nombre']:
+                    cliente = cargue_tools.create_find_res_partner(
+                        False,
+                        cliente_compañia.id,
+                        row['cliente_individuo_nombre'],
+                        row['cliente_individuo_direccion'],
+                        row['cliente_individuo_telefono'],
+                        row['cliente_individuo_cel'],
+                        row['cliente_individuo_email'],
+                        row['cliente_individuo_web'],
+                        row['cliente_individuo_puesto_trabajo'],
+                    )
